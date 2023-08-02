4b8648f569485df398bb5a00651f5e3fea68ced9
suraj <surajlalwani890@gmail.com>
2023-08-02 21:16:04 +0530

    third

---
+++ terraform-eks-script/var.tf
@@ -0,0 +1,19 @@
+variable aws_access_key{
+default = "<ACCESS_KEY>"
+}
+
+
+variable aws_secret_key{
+default ="<SECRET_ACCESS_KEY>"
+}
+
+
+variable "subnet_id_1" {
+type = string
+default = "<SUBNET_ID>"
+}
+
+variable "subnet_id_2" {
+type = string
+default = "<SUBNET_ID>"
+}
